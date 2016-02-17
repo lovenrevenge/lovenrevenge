6825687c4e55e84032d4ccaf82df47e96a28c7db
Sydney Selch <sydney_selch@Sydneys-MacBook-Pro.local>
2016-02-17 00:49:16 -0500

    line moved js

--- js/push-page.js
+++ js/push-page.js
@@ -2,7 +2,8 @@ $('.mobile-nav, .dt-nav').localScroll();
 
 var $links = $('.mobile-nav a');
 
-$links.on('click', function () {
+      $links.on('click', function () {
      $('.nav-check').prop('checked', false);
 });
+    
 
